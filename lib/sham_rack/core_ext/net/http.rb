353e8196651b158550e2eab108f47a81bf21616f
Mike Williams <mdub@dogbiscuit.org>
2009-11-27 22:33:20 +1100

    Tweak.

--- lib/sham_rack/core_ext/net/http.rb
+++ lib/sham_rack/core_ext/net/http.rb
@@ -4,12 +4,12 @@ require "sham_rack/http"
 
 class << Net::HTTP  
 
-  alias :new_without_shamrack :new
+  alias :new_without_sham_rack :new
   
   def new(address, port = nil, *proxy_args)
     port ||= Net::HTTP.default_port
     rack_app = ShamRack.application_for(address, port)
-    http_object = new_without_shamrack(address, port, *proxy_args)
+    http_object = new_without_sham_rack(address, port, *proxy_args)
     if rack_app
       http_object.extend(ShamRack::HTTP::Extensions)
       http_object.rack_app = rack_app
