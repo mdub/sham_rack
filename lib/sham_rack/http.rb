3d34c8c9f49df5abfbefc6be2a893b792d4cb982
Mike Williams <mdub@dogbiscuit.org>
2009-11-27 23:35:08 +1100

    Remove some accessors that are now redundant.

--- lib/sham_rack/http.rb
+++ lib/sham_rack/http.rb
@@ -6,9 +6,6 @@ module ShamRack
 
       attr_accessor :rack_app
 
-      attr_accessor :read_timeout, :open_timeout
-      attr_accessor :use_ssl,:key, :cert, :ca_file, :ca_path, :verify_mode, :verify_callback, :verify_depth, :cert_store
-
       def start
         yield self
       end
