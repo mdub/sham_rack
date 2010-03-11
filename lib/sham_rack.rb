d4afd5489d0f2bbaad032fb50821890977e18b9f
Mike Williams <mdub@dogbiscuit.org>
2010-03-11 13:28:32 +1100

    Add StubWebService.

--- lib/sham_rack.rb
+++ lib/sham_rack.rb
@@ -5,4 +5,5 @@
 module ShamRack; end
 
 require "sham_rack/core_ext/net/http"
+require "sham_rack/registry"
 require "sham_rack/version"
--- spec/sham_rack_spec.rb
+++ spec/sham_rack_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require "spec_helper"
 
 require "sham_rack"
 require "open-uri"
